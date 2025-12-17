60e3f1d73631f85cb2d8c3934f56bfc718a5af8d
Aaaaa0850 <oshama0850@gmail.com>
2025-12-17 23:48:20 +0900

    KVとDrizzle関連の設定

---
+++ drizzle/schema.ts
@@ -0,0 +1,8 @@
+import { sql } from "drizzle-orm";
+import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+
+export const users = sqliteTable("users", {
+  id: text("id").primaryKey(),
+  createdAt: integer("created_at", { mode: "timestamp" }).default(sql`CURRENT_TIMESTAMP`),
+});
+
