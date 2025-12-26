e6d0aaf73fec08a6f5600b596c881f3e02f65f06
Aaaaa0850 <oshama0850@gmail.com>
2025-12-27 02:51:47 +0900

    drizzle設定ファイルのコメントを削除

--- drizzle/drizzle.config.ts
+++ drizzle/drizzle.config.ts
@@ -1,21 +1,6 @@
-/*import type { Config } from "drizzle-kit";
-
-
-export default {
-  schema: "./drizzle/schema.ts",
-  out: "./drizzle/migrations",
-  driver: "d1-http",
-  dialect: "sqlite",
-  dbCredentials: {
-    accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
-    databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
-    token: process.env.CLOUDFLARE_D1_TOKEN!,
-  },
-} satisfies Config;*/
 import type { Config } from "drizzle-kit";
 import dotenv from "dotenv";
 
-// ENV に応じて読み込む環境変数ファイルを切替え
 const currentEnv = process.env.ENV ?? "development";
 console.log(`Current environment: ${currentEnv}`);
 dotenv.config({
